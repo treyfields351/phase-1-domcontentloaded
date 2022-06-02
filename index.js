f3678cb2bb386852840fcc0cdcc3065ce0645042
Trey <treyfields351@gmail.com>
2022-06-02 13:50:19 -0400

    commit tree 53bff1ad1dbce25ab349a6a54e4f13781eaa601a

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Your code goes here
+// document.addEventListener("DomContentLoaded", function ()
+// {
+//     updateDOM()
+// });
+
+// function updateDOM()
+// {
+//     document.getElementById("text")
+//         .innerHTML = "This is really cool!";
+// }
+
+document.addEventListener( "DOMContentLoaded", function () {
+    updateDOM()
+  } );
+  
+  
+  function updateDOM() {
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
